4f374de11ed0c6edd3d9fe9eb81c158af58fa861
darkfeatherlan <87290324+darkfeatherlan@users.noreply.github.com>
2022-10-29 18:16:38 +0800

    Update setup-k8s-master.sh

--- setup-k8s-master.sh
+++ setup-k8s-master.sh
@@ -10,4 +10,4 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f rbac.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
-kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-systemd:kubernetes-dashboard
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
