7834d045c071b66e4ccea7f82879f7ae409f195b
darkfeatherlan <87290324+darkfeatherlan@users.noreply.github.com>
2024-10-30 23:28:28 +0800

    Create setup-k8s-master-v1.31.sh
    k8s-v1.31.2

---
+++ setup-k8s-master-v1.31.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+# Initialize a kubernetes cluster on the current node.
+#
+KUBE_VERSION=1.31.2
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=$KUBE_VERSION --ignore-preflight-errors=NumCPU
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
